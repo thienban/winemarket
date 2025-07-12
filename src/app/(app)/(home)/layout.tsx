53d636acc8262efae368401815182bb149263575
Thien Bui <thienban91@hotmail.fr>
2025-07-13 00:58:42 +0200

    add search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,42 @@
+import configPromise from "@payload-config"
+import { getPayload } from "payload";
+import { Category } from "@/payload-types";
+
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { SearchFilters } from "./search-filters";
 
 interface Props {
     children: React.ReactNode
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = async ({ children }: Props) => {
+
+    const payload = await getPayload({ config: configPromise });
+
+    const data = await payload.find({
+      collection: "categories",
+      depth: 1,
+      pagination: false,
+      where: {
+        parent: {
+          exists: false
+        }
+      }
+    })
+
+    const formattedData = data.docs.map((doc: any) => ({
+      ...doc,
+      subcategories: (doc.subcategories?.docs ?? []).map((doc: Category) => ({
+          ...(doc as Category),
+          subcategories: undefined
+      }))    
+    }));
+
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
+            <SearchFilters data={formattedData}/>
             <div className="flex-1 bg-[#F4F4F0]">
                 {children}
             </div>
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,23 +1,13 @@
-import configPromise from "@payload-config"
-import { getPayload } from "payload";
-
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
 
-export default async function Home() {
-
-  const payload = await getPayload({ config: configPromise });
-
-  const data = await payload.find({
-    collection: "categories",
-  })
+export default function Home() {
 
   return (
     <div className="p-4">
-      {JSON.stringify(data, null, 2)}
       <div className="flex flex-col gap-y-4">
         <p className="text-rose-400">Test</p>
         <div>
