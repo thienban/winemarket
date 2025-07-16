59ca5f4f8c96d70abe7638b8461a34d582340350
Thien Bui <thienban91@hotmail.fr>
2025-07-16 22:46:21 +0200

    add category route and responsive

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import { useRef, useState } from "react"
+import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
+
 import { useDropdownPosition } from "./use-dropdown-position"
 import { SubcategoryMenu } from "./subcategory-menu"
 import { CustomCategory } from "../type"
@@ -32,18 +34,31 @@ export const CategoryDropdown = ({ category, isActive, isNavigationHovered }: Pr
 
     const dropdownPosition = getDropdownPosition()
 
+    const toggleDropdown = () => {
+        if (category.subcategories?.docs?.length) {
+            setIsOpen(!isOpen)
+        }
+        setIsOpen(!isOpen)
+    }
+
     return (
         <div className="relative"
             ref={dropdownRef}
             onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}>
+            onMouseLeave={onMouseLeave}
+            onClick={toggleDropdown}
+        >
             <div className="relative">
                 <Button
                     variant="elevated"
                     className={cn("h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
                         isActive && !isNavigationHovered && "bg-white border-primary",
                         isOpen && "bg-white border-primary shadow-[4px_4px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]"
-                    )}>{category.name}
+                    )}
+                >
+                    <Link href={`/${category.slug === "all" ? "" : category.slug}`}>
+                        {category.name}
+                    </Link>
                 </Button>
                 {category.subcategories && category.subcategories.length > 0 && (
                     <div className={cn("opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
