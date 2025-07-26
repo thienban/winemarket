79d5dd5ea8c630eb7504f653e82b87d3bb4295ce
Thien Bui <thienban91@hotmail.fr>
2025-07-27 00:46:56 +0200

    add category pages

--- src/modules/home/ui/components/search-filters/categories.tsx
+++ src/modules/home/ui/components/search-filters/categories.tsx
@@ -1,20 +1,23 @@
 "use client"
 
 import { ListFilterIcon } from "lucide-react"
+import { useParams } from "next/navigation"
 import { useEffect, useRef, useState } from "react"
 
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
-
-import { CategoryDropdown } from "./category-dropdown"
-import { CategoriesSidebar } from "./categories-sidebar"
 import { CategoriesGetManyOutput } from "@/modules/categories/type/type"
+
+import { CategoriesSidebar } from "./categories-sidebar"
+import { CategoryDropdown } from "./category-dropdown"
 
 interface Props {
     data: CategoriesGetManyOutput
 }
 
 export const Categories = ({ data }: Props) => {
+
+    const params = useParams()
 
     const containerRef = useRef<HTMLDivElement>(null)
     const mesureRef = useRef<HTMLDivElement>(null)
@@ -24,7 +27,8 @@ export const Categories = ({ data }: Props) => {
     const [isAnyHovered, setIsAnyHovered] = useState(false)
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
-    const activeCategory = "all"
+    const categoryParam = params.category as string | undefined
+    const activeCategory = categoryParam || "all"
 
     const activeCategoryIndex = data.findIndex((category) => category.slug === activeCategory)
     const isActiveCategoryHidden = activeCategoryIndex >= visibleCount && activeCategoryIndex !== -1
@@ -97,8 +101,9 @@ export const Categories = ({ data }: Props) => {
                 }
                 <div ref={viewAllRef} className="shrink-0">
                     <Button
+                        variant="elevated"
                         className={cn("h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-primary hover:border-primary text-black",
-                        isActiveCategoryHidden && !isAnyHovered && "bg-white border-")}
+                            isActiveCategoryHidden && !isAnyHovered && "bg-white border-")}
                         onClick={() => setIsSidebarOpen(true)}
                     >
                         View All
