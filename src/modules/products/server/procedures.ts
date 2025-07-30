613b5988657fd630d476615a23047b33cf4a8c94
Thien Bui <thienban91@hotmail.fr>
2025-07-30 15:18:30 +0200

    fix product filter

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -13,16 +13,23 @@ export const productsRouter = createTRPCRouter({
             })
         ).query(async ({ ctx, input }) => {
 
-            const where: Where = {}
-
-            if (input.minPrice) {
-                where.price = {
-                    greater_than_equal: input.minPrice
-                }
+            const where: Where = {
+                price: {}
             }
 
-            if (input.maxPrice) {
+            if (input.minPrice && input.maxPrice) {
                 where.price = {
+                    greater_than_equal: input.minPrice,
+                    less_than_equal: input.maxPrice,
+                }
+            } else if (input.minPrice) {
+                where.price = {
+                    ...where.price,
+                    greater_than_equal: input.minPrice
+                }
+            } else if (input.maxPrice) {
+                where.price = {
+                    ...where.price,
                     less_than_equal: input.maxPrice
                 }
             }
